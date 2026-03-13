1124bb83a70eef0259a334cd84a0e3a0bb7b17aa
Rafael Maldonado <rmaldonado@nimbox.com>
2026-03-13 11:23:35 -0400

    Agregar página web "Mi Vaquita Apureña" con HTML, CSS y JavaScript, incluyendo imágenes y funcionalidad de carrusel

---
+++ miVaquitaApureña/script.js
@@ -0,0 +1,36 @@
+let index = 0;
+
+function update(){
+
+const track = document.getElementById("track");
+track.style.transform = `translateX(-${index*100}%)`;
+
+}
+
+function next(){
+
+const total = document.querySelectorAll(".product").length;
+
+index++;
+
+if(index >= total){
+index = 0;
+}
+
+update();
+
+}
+
+function prev(){
+
+const total = document.querySelectorAll(".product").length;
+
+index--;
+
+if(index < 0){
+index = total - 1;
+}
+
+update();
+
+}
